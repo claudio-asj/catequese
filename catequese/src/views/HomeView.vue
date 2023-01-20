efd013199c7d40964d7b8ef62b2f003ff9bb8d29
YanGraim <yangraim@gmail.com>
2023-01-20 11:33:04 -0300

    Testando input

--- catequese/src/views/HomeView.vue
+++ catequese/src/views/HomeView.vue
@@ -1,9 +1,28 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
   <main>
-    oi
+    <input type="text" placeholder="Digite seu nome" v-model="exemplo">
+    <p>Essa mensagem é {{ exemplo }}</p>
+    <input type="text" v-model="cadastro.nome">
+    <input type="number" v-model="cadastro.idade">
+    <button @click="button">Enviar</button>
   </main>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      exemplo: null,
+      cadastro: {
+        nome: null,
+        idade: null
+      }
+    }
+  },
+  methods: {
+    button() {
+      console.log('alguma coisa...')
+    }
+  }
+}
+</script>
